94d6bde00863c2580697d86818875389dc64597e
pandah-404 <fajarandhika0207@gmail.com>
2025-07-26 21:59:21 +0700

    Update

--- app/data/portfolio.ts
+++ app/data/portfolio.ts
@@ -24,22 +24,22 @@ export const frontendProjects = [
 export const backendProjects = [
     {
         id: 1,
-        name: "E-commerse System",
+        name: "E-commerce System",
         color: '#32a8a8',
         image: be,
         imageDescription: "E-commerce System",
         url: "https://github.com/daisynotfound/shopee",
         description: "E-commerce with 2 role (Admin and User)",
         technologies: ['Laravel', 'Php']
-    }
-    // {
-    //     id: 2,
-    //     name: "Teams Platform",
-    //     color: '#EA2829',
-    //     image: shirogane,
-    //     imageDescription: "Bermel Fest Image",
-    //     url: "https://shiroganeteams.vercel.app/",
-    //     description: "This is my school project",
-    //     technologies: ['nextjs', 'tailwindcss', 'ts']
-    // },
+    },
+    {
+        id: 2,
+        name: "E-commerce",
+        color: '#EA2829',
+        image: shirogane,
+        imageDescription: "E-commerce",
+        url: "https://github.com/daisynotfound/DaisyShop",
+        description: "Same e-commerce but you can check your product status on your cart",
+        technologies: ['expressjs',  'nodejs']
+    },
 ]
